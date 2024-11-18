1c0658021582863fe3646922c237b1d02a7d4a0a
Shapik <egorshapovalov06@gmail.com>
2024-11-18 17:24:59 +0300

    update lab2 ЛР?

--- ЛР7/2lab.py
+++ ЛР7/2lab.py
@@ -6,6 +6,7 @@ lst = [[random.randint(-1000,1000) for n in range(N)] for m in range(M)]
 
 for m in range(M):
     print(lst[m])
-
-for x in range(M):
-    print(*lst[x][::(1 if 1+x%2 == 1 else -1)])
+for n in range(N):
+    print(n+1, "_"*20)
+    for m in list(range(M))[::(1 if 1+n%2 == 1 else -1)]:
+        print(lst[m][n])
